aa7d388a1128803eb1389a5b3a239c657a62741b
Constanza Lancieri <constanza.lancieri@brubank.com>
2025-02-15 15:54:10 -0300

    create project

---
+++ eventyApp/eventyApp/eventyApp.swift
@@ -0,0 +1,17 @@
+//
+//  eventyAppApp.swift
+//  eventyApp
+//
+//  Created by Constanza Lancieri on 15/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct eventyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
